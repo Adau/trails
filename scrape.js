ff092194f39c12dccb0bb114eeca97375b6bbfe7
Adrien <adrien.dau@gmail.com>
2018-03-26 09:52:17 +0200

    Ajouter une fonction pour formater les données

--- scrape.js
+++ scrape.js
@@ -5,42 +5,64 @@ const competitions = require('./competitions')
 const moment = require('moment')
 moment.locale('fr')
 
+const format = async (value, type = 'string') => {
+  switch (type) {
+    case 'boolean':
+      value = !!value
+      break
+
+    case 'number':
+      value = value.trim() || '0'
+      value = value.match(/\d+(,\d+)?/)[0]
+      value = parseFloat(value.replace(',', '.'))
+      break
+
+    case 'date':
+      value = moment(value, 'dddd D MMMM YYYY HH:mm:ss')
+      break
+
+    case 'string':
+    default:
+      value = value.trim()
+      break
+  }
+
+  return value
+}
+
 const getData = async (browser, competition) => {
   const page = await browser.newPage()
   await page.goto(competition.url)
   await page.waitFor('body')
 
   await page.exposeFunction('moment', moment)
+  await page.exposeFunction('format', format)
 
   const data = await page.evaluate(async competition => {
     const nodes = document.querySelectorAll('.competition')
 
     for (let node of nodes) {
-      const name = node.querySelector('.competition-name').innerText.trim()
+      const name = await format(node.querySelector('.competition-name').innerText, 'string')
 
       if (name != competition.name) {
         continue
       }
 
-      let date = node.querySelector('.date').innerText.trim()
-      let time = node.querySelector('.time').innerText.trim()
-      date = await moment(`${date} ${time}`, 'dddd D MMMM YYYY HH:mm:ss')
+      let date = await format(node.querySelector('.date').innerText, 'string')
+      let time = await format(node.querySelector('.time').innerText, 'string')
+      date = await format(`${date} ${time}`, 'date')
 
-      let distance = node.querySelector('.span6 > .stats-container .stat.summary span').innerText.trim()
-      distance = distance.match(/\d+(,\d+)?/)[0]
-      distance = parseFloat(distance.replace(',', '.'))
+      let distance = node.querySelector('.span6 > .stats-container .stat.summary span').innerText
+      distance = await format(distance, 'number')
 
-      let elevation = node.querySelector('.span6 > .stats-container .stat.summary span:last-child').innerText.trim()
-      elevation = elevation.match(/\d+(,\d+)?/)[0]
-      elevation = parseFloat(elevation.replace(',', '.'))
+      let elevation = node.querySelector('.span6 > .stats-container .stat.summary span:last-child').innerText
+      elevation = await format(elevation, 'number')
 
-      let remainingPlaces = node.querySelector('.remaining-seats').innerText.trim()
-      remainingPlaces = remainingPlaces.match(/\d+(,\d+)?/)[0]
-      remainingPlaces = parseFloat(remainingPlaces.replace(',', '.'))
+      let remainingPlaces = node.querySelector('.remaining-seats').innerText
+      remainingPlaces = await format(remainingPlaces, 'number')
 
-      let totalPlaces = node.querySelector('.participant-limit').innerText.trim()
-      totalPlaces = totalPlaces.match(/\d+(,\d+)?/)[0]
-      totalPlaces = parseFloat(totalPlaces.replace(',', '.'))
+      let totalPlaces = node.querySelector('.participant-limit').innerText
+      totalPlaces = await format(totalPlaces, 'number')
 
       let isOpen = !!node.querySelector('.competition-buttons')
 
